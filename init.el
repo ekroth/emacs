62c44f592d995a227e94b38582b9d3720d9a43b4
Andrée Ekroth <andree.ekroth@gmail.com>
2015-02-03 21:15:56 +0100

    osx: init

--- init.el
+++ init.el
@@ -29,3 +29,37 @@
 
 ;; load modes
 (load "~/.emacs.d/config/modes")
+
+;; Aquamacs
+(custom-set-variables
+;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aquamacs-customization-version-id 305 t)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(default-frame-alist
+    (quote
+     ((menu-bar-lines . 0)
+      (tool-bar-lines . 0)
+      (vertical-scroll-bars))))
+ '(font-use-system-font t)
+ '(global-linum-mode t)
+ '(gud-gdb-command-name "gdb --annotate=1")
+ '(large-file-warning-threshold nil)
+ '(ns-tool-bar-display-mode nil t)
+ '(ns-tool-bar-size-mode nil t)
+ '(tabbar-mode t nil (tabbar))
+ '(visual-line-mode nil t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; OSX
+(setq default-input-method "MacOSX")
+(setq mac-command-modifier 'meta
+      mac-option-modifier nil
+      mac-allow-anti-aliasing t
+      mac-command-key-is-meta t
+      x-select-enable-clipboard t)
