6b41fb595bdf2bb3e77d1c5fa2f9dfb6f66218a5
Andrée Ekroth <andree.ekroth@gmail.com>
2015-03-19 14:25:16 +0100

    osx: move to file, disable custom-opts

---
+++ config/osx.el
@@ -0,0 +1,32 @@
+;; OSX
+
+; Disable options saving
+(setq aquamacs-save-options-on-quit nil)
+
+; Disable tabs
+(setq tabbar-mode nil)
+
+; Use system font
+(setq font-use-system-font t)
+
+; This does something to the font as well..
+(setq default-frame-alist
+    (quote
+     ((menu-bar-lines . 0)
+      (tool-bar-lines . 0)
+      (vertical-scroll-bars))))
+
+; Input mode
+(setq default-input-method "MacOSX")
+
+; Meta key
+(setq mac-command-modifier 'meta)
+
+; Disable option key
+(setq mac-option-modifier nil)
+
+; Allow anti alias
+(setq mac-allow-anti-aliasing t)
+
+; Enable clipboard
+(setq x-select-enable-clipboard t)
