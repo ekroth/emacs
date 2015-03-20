f46799509b52c1bedba514adc09915bba48a8fb0
Andrée Ekroth <andree.ekroth@gmail.com>
2015-03-20 17:18:48 +0100

    modes/perspective: add helper function

--- config/modes/perspective.el
+++ config/modes/perspective.el
@@ -8,6 +8,19 @@
 ; - `i`  --  `persp-import`: Import a given perspective from another frame.
 ; - `n`, `<right>`  --  `persp-next` : Switch to next perspective
 ; - `p`, `<left>`   --  `persp-prev`: Switch to previous perspective
+; persp-switch NAME
 
 (use-package perspective
-    :init (persp-mode 1))
+  :config
+  (persp-mode 1)
+
+  (defun persp-ext-switch-index (index)
+    (let ((target (elt (persp-all-names) index)))
+      (when target
+        (persp-switch target))))
+
+  (dotimes (i 6)
+    (global-set-key
+     (kbd (concat "^C " (number-to-string (1+ i))))
+     (lexical-let ((lex-i i))
+       (lambda () (interactive) (persp-ext-switch-index lex-i))))))
