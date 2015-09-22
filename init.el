5656509af5f6cbaa53f74bc804525305d3ae7ee3
Andrée Ekroth <andree.ekroth@gmail.com>
2015-09-22 12:53:03 +0200

    init: global backup dir with dates

--- init.el
+++ init.el
@@ -16,8 +16,14 @@
 (add-to-list 'load-path "~/.emacs.d/modes")
 (add-to-list 'load-path "/usr/share/emacs/site-lisp")
 
-;; global backup directory
-(setq backup-directory-alist `(("." . "~/.emacs.d/backups")))
+;; Save all tempfiles in $TMPDIR/emacs$UID/
+(defconst emacs-tmp-dir (format "%s/%s%s/" temporary-file-directory "emacs" (user-uid)))
+(setq backup-directory-alist
+      `((".*" . ,emacs-tmp-dir)))
+(setq auto-save-file-name-transforms
+      `((".*" ,emacs-tmp-dir t)))
+(setq auto-save-list-file-prefix
+      emacs-tmp-dir)
 
 ;; no startup buffers
 (setq inhibit-startup-screen t)
