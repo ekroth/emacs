2354157cd14065e72a3698e60ed6525c9941326e
Andrée Ekroth <andree.ekroth@gmail.com>
2015-09-22 12:52:49 +0200

    modes/scala: add sbt

--- config/modes/scala.el
+++ config/modes/scala.el
@@ -1,4 +1,5 @@
 (use-package scala-mode2
   :commands scala-mode
   :config
-  (add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode)))
+  (add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
+  (add-to-list 'auto-mode-alist '("\\.sbt$" . scala-mode)))
